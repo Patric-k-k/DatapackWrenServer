affac0ae261e45a4dc15750863d843021ac940b7
Patric-k-k <beepbooppk@gmail.com>
2026-07-31 15:22:59 -0400

    Remove Debug

--- run.sh
+++ run.sh
@@ -2,12 +2,6 @@
 #run.sh
 export PATH=/home/pk/.cargo/bin:/home/pk/miniconda3/bin:/home/pk/miniconda3/condabin:/home/pk/.local/bin:/home/pk/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin
 
-echo "---- run.sh ----" >> ./tmp/runlog.txt
-date >> ./tmp/runlog.txt
-pwd >> ./tmp/runlog.txt
-id >> ./tmp/runlog.txt
-whoami >> ./tmp/runlog.txt
-echo "PATH=$PATH" >> ./tmp/runlog.txt
 
 bash ./build.sh ./script.cpp /home/pk/Desktop/cppdatapacks/apps/local2/app
 
