f3175594e11a59fa679b6a27c81420e999a4f733
Patric-k-k <beepbooppk@gmail.com>
2026-07-31 18:53:58 -0400

    Luau

--- runluau.php
+++ runluau.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 $data = json_decode(file_get_contents('php://input'),true);
 
 if ($data["passcode"] != "9828") {
@@ -14,7 +16,7 @@ foreach ($data["pages"] as $page) {
 
 file_put_contents("./script.luau",$script);
 
-echo shell_exec("luau /script.luau");
+popen("/home/pk/.local/bin/luau ./script.luau > ./outputluau.txt","r");
 
 echo file_get_contents("./outputluau.txt");
 ?>
