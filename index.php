eb12bb5e8126671e74b49a6068f2c9ddb90b87b0
Patric-k-k <beepbooppk@gmail.com>
2026-07-31 11:32:23 -0400

    do stuff

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body class="bg-zinc-900">
+<p class="text-3xl font-bold underline text-white text-center">
+this is a <span class="text-green-500">horrible idea</span>.
+</p>
+<p class="text-white text-center">Arbitrary Code Execution intended!<span class="text-red-500">*</span></p>
+<p class="text-red-500/25 text-center text-sm">*but not in a dangerous way</p>
+</body>
+</html>
